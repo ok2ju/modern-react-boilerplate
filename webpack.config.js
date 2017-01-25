096c893aaa3160fd306955a0593edf6325c2ef50
ok2ju <soulSpringg@gmail.com>
2017-01-25 10:29:45 +0300

    add dev-sever, hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-import path from 'path';
+const { resolve } = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
+  context: resolve(__dirname, 'src'),
+  devtool: 'inline-source-map',
   entry: {
-    jsx: './index.js',
-    html: './index.html',
+    main: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      './index.js'
+    ],
     vendor: [
       'react',
       'react-dom',
-      'react-redux',
-      'react-router',
-      'redux',
     ],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    filename: '[chunkhash].[name].js',
+    path: resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
+        use: [
+          'babel-loader',
+        ],
         exclude: /node_modules/,
-        use: [{
-          loader: 'react-hot',
-        }, {
-          loader: 'babel-loader',
-        }],
-      },
-      {
-        test: /\.html$/,
-        loader: 'file?name=[name].[ext]',
       },
     ],
   },
-  resolve: {
-    extensions: ['js', 'jsx'],
-  },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor'
+    }),
     new webpack.DefinePlugin({
       'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
     }),
   ],
   devServer: {
-    contentBase: path.resolve(__dirname, 'src'),
     hot: true,
+    contentBase: resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
 };
