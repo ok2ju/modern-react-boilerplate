1489307765f21c81f3590f17157a9827e3d698f1
ok2ju <soulSpringg@gmail.com>
2017-01-26 10:52:15 +0300

    add eslint cli support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import 'index.html';
+import './index.html';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
