bdcb3d742dc9dc44ac5c280e62155525298c510a
ok2ju <soulSpringg@gmail.com>
2017-02-21 16:30:32 +0300

    add redux-devtools extension support

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,7 +2,13 @@ import { createStore } from 'redux';
 import rootReducer from '../reducers';
 
 export default (initialState) => {
-  const store = createStore(rootReducer, initialState);
+  /* eslint-disable no-underscore-dangle */
+  const store = createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
+  /* eslint-enable */
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
